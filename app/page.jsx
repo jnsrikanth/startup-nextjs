61bf01eda5b76ccad3a08f0ed91062ca00f00475
Srikanth J <jnsrikanth@yahoo.com>
2023-06-11 00:37:21 +0530

    Comment did not work - so just removed it.

--- app/page.jsx
+++ app/page.jsx
@@ -19,9 +19,6 @@ export default function Home() {
       <Features />
       <Video />
       <Brands />
-      <!--
-      <AboutSectionOne />
-      -->
       <AboutSectionTwo />
       <Testimonials />
       <Blog />
