f456851255172161745549a0a970efbcd0c20bfd
Srikanth J <jnsrikanth@yahoo.com>
2023-06-11 22:13:34 +0530

    Updated Newsletter Text

--- components/Contact/NewsLatterBox.jsx
+++ components/Contact/NewsLatterBox.jsx
@@ -5,11 +5,10 @@ const NewsLatterBox = () => {
       data-wow-delay=".2s"
     >
       <h3 className="mb-4 text-2xl font-bold leading-tight text-black dark:text-white">
-        Subscribe to receive future updates
+        Stay Informed:Subscribe to receive future updates
       </h3>
       <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base font-medium leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
-        Lorem ipsum dolor sited Sed ullam corper consectur adipiscing Mae ornare
-        massa quis lectus.
+        Be the first to know about our latest investments, industry insights, and upcoming events. Subscribe to our newsletter for regular updates delivered straight to your inbox
       </p>
       <form>
         <input
