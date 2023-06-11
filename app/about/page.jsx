a0ff17347a9979a70722d33b06319485fbf47c09
Srikanth J <jnsrikanth@yahoo.com>
2023-06-11 23:16:16 +0530

    Updated About Page Header

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,8 +6,8 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Empowering Growth, Creating Value: About Altius PE Holdings"
+        description="Driving growth, creating value. Our strategic approach and ESG commitment fuel sustainable success."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
